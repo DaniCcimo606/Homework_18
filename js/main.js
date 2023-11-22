2dda4ff3c5c681acad0e08ef97fdc73d1af1e909
DaniCcimo <bessmertnyj228@gmail.com>
2023-11-22 12:49:13 +0200

    Change quotes

--- js/main.js
+++ js/main.js
@@ -2,23 +2,23 @@ let result;
 
 do {
 
-    let operation = prompt(`Что вы хотите сделать? (Сумма: " + ", разница: " - ", умножение: " * ", деление: " / ");`);
+    let operation = prompt('Что вы хотите сделать? (Сумма: " + ", разница: " - ", умножение: " * ", деление: " / ");');
     
     if (operation !== null) {
-        let firstNum = prompt(`Введите первое число:`);
-        let secondNum = prompt(`Введите второе число:`);
+        let firstNum = prompt('Введите первое число:');
+        let secondNum = prompt('Введите второе число:');
         
-        if (operation == `+`) {
+        if (operation == '+') {
             result = firstNum + secondNum;
-        } else if (operation == `-`) {
+        } else if (operation == '-') {
             result = firstNum - secondNum;
-        } else if (operation == `*`) {
+        } else if (operation == '*') {
             result = firstNum * secondNum;
-        } else if (operation == `/`) {
+        } else if (operation == '/') {
             result = firstNum / secondNum;
         }
     } else {
-        alert(`Ok, goodbye :)`);
+        alert('Ok, goodbye :)');
         break;
     }
     
@@ -29,29 +29,29 @@ do {
 
 
 
-// let operation = prompt(`Что вы хотите сделать? (Сумма - "+", разница - "-", умножение - "*", деление - "/")`);
+// let operation = prompt('Что вы хотите сделать? (Сумма - "+", разница - "-", умножение - "*", деление - "/")');
 
-// if (operation == `+` || operation == `-` || operation == `*` || operation == `/`) {
-//     let firstNum = +prompt(`Введите первое число:`);
-//     let secondNum = +prompt(`Введите второе число:`);
+// if (operation == '+' || operation == '-' || operation == '*' || operation == '/') {
+//     let firstNum = +prompt('Введите первое число:');
+//     let secondNum = +prompt('Введите второе число:');
 
 //     if (firstNum !== null && secondNum !== null) {
-//         if (operation == `+`) {
+//         if (operation == '+') {
 //             result = firstNum + secondNum;
-//         } else if (operation == `-`) {
+//         } else if (operation == '-') {
 //             result = firstNum - secondNum;
-//         } else if (operation == `*`) {
+//         } else if (operation == '*') {
 //             result = firstNum * secondNum;
-//         } else if (operation == `/`) {
+//         } else if (operation == '/') {
 //             result = firstNum / secondNum;
 //         }
 //     } else {
-//         alert(`Ok, goodbye :)`);
+//         alert('Ok, goodbye :)');
 //     }
 // } else if (operation === null) {
-//     alert(`Ok, goodbye :)`);
+//     alert('Ok, goodbye :)');
 // } else {
-//     alert(`Sorry, invalid character :(`);
+//     alert('Sorry, invalid character :(');
 // }
 
 // console.log(result);
@@ -61,21 +61,21 @@ do {
 
 // let result = 0;
 
-// let operation = prompt(`Что вы хотите сделать? (Сумма - "+", разница - "-", умножение - "*", деление - "/")`);
+// let operation = prompt('Что вы хотите сделать? (Сумма - "+", разница - "-", умножение - "*", деление - "/")');
 
-// let firstNum = +prompt(`Введите первое число:`);
-// let secondNum = +prompt(`Введите второе число:`);
+// let firstNum = +prompt('Введите первое число:');
+// let secondNum = +prompt('Введите второе число:');
 
-// if (operation == `+`) {
+// if (operation == '+') {
 //     result = firstNum + secondNum;
-//     main.innerHTML = `${firstNum} + ${secondNum} = ${result}`;
-// } else if (operation == `-`) {
+//     main.innerHTML = '${firstNum} + ${secondNum} = ${result}';
+// } else if (operation == '-') {
 //     result = firstNum - secondNum;
-//     main.innerHTML = `${firstNum} - ${secondNum} = ${result}`;
-// } else if (operation == `*`) {
+//     main.innerHTML = '${firstNum} - ${secondNum} = ${result}';
+// } else if (operation == '*') {
 //     result = firstNum * secondNum;
-//     main.innerHTML = `${firstNum} * ${secondNum} = ${result}`;
-// } else if (operation == `/`) {
+//     main.innerHTML = '${firstNum} * ${secondNum} = ${result}';
+// } else if (operation == '/') {
 //     result = firstNum / secondNum;
-//     main.innerHTML = `${firstNum} / ${secondNum} = ${result}`;
+//     main.innerHTML = '${firstNum} / ${secondNum} = ${result}';
 // }
